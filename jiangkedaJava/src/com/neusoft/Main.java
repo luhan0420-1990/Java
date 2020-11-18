b78a3fbf273028c5f4b1e18f4f4b8aa07a9b6992
luhan0420-1990 <2549438729@qq.com>
2020-11-18 15:10:50 +0800

    测试

--- jiangkedaJava/src/com/neusoft/Main.java
+++ jiangkedaJava/src/com/neusoft/Main.java
@@ -3,7 +3,7 @@ package com.neusoft;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hh");
+        System.out.println("hhhh");
 	// write your code here
     }
 }
